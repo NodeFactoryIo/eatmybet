04ef2a40e5e2b715f0db1ae0530c078898b80d6b
Željko Dragosavac <zeljko.dragosavac@m2bit.com>
2018-06-11 19:19:34 +0200

    First draft - scrapper fifa

--- backend/routes.js
+++ backend/routes.js
@@ -1,11 +1,19 @@
 import { Router } from 'express';
-
+import Scrapper from './services/scrapper';
 
 export default () => {
   const api = Router();
 
   api.get('/', (req, res) => {
     res.send('You have reached backend server!');
+  });
+
+  api.get('/fixtures-01', async(req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    
+    let ret = await Scrapper.ScrapFifa(req, res);
+    res.json(ret);
   });
 
   return api;
