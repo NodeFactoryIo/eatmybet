ce83c2d5cd9d2742b95b381c836dd9f0d4846f5f
morrigan <belma.dzi@gmail.com>
2018-06-12 20:45:45 +0200

    Migrate first matches

--- migrations/2_contract_migration.js
+++ migrations/2_contract_migration.js
@@ -1,6 +1,22 @@
 /* eslint-disable no-undef */
 let EatMyBet = artifacts.require('./EatMyBet.sol');
+const matches = require('./matches.json');
 
 module.exports = function(deployer) {
-  deployer.deploy(EatMyBet);
+  deployer.deploy(EatMyBet)
+    .then(() => EatMyBet.deployed())
+    .then(contract => {
+      for (let i = 0; i < matches.length; i++) {
+        const gameId = matches[i].gameId;
+        const home = matches[i].homeTeamNameShort;
+        const away = matches[i].awayTeamNameShort;
+        const startTime = new Date(matches[i].dateTime).getTime();
+
+        contract.storeMatch(home, away, gameId, startTime)
+          .catch(log => {
+            console.log('Error ocurred while storing match: ', log);
+            break;
+          });
+      }
+    });
 };
