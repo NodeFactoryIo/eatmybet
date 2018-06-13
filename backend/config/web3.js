cc7bc4b76cfa30fdac477b7c3c6e1d45ad9161dc
Marin Petrunić <marin.petrunic@gmail.com>
2018-06-13 12:24:00 +0200

    updtes ethereum url config

--- backend/config/web3.js
+++ backend/config/web3.js
@@ -4,13 +4,12 @@ dotenv.config();
 
 const env = process.env;
 
-const networkHost = env.ETHEREUM_NETWORK_HOST || 'ethereum';
-const networkPort = 8545;
+const network = env.ETHEREUM_NETWORK_URL || 'http://ethereum:8545';
 
 export default class Web3Config {
 
   static url() {
-    return `http://${networkHost}:${networkPort}`;
+    return network;
   }
 
 }
