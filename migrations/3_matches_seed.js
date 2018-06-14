f8f6b6a7135c3cc291d2ed56f0001f6be79027a7
Marin Petrunić <marin.petrunic@gmail.com>
2018-06-14 14:06:25 +0200

    changes match structure and tests

--- migrations/3_matches_seed.js
+++ migrations/3_matches_seed.js
@@ -3,19 +3,19 @@ let EatMyBet = artifacts.require('./EatMyBet.sol');
 const matches = require('./matches.json');
 
 module.exports = function(deployer) {
-  EatMyBet.deployed()
-    .then(contract => {
-      for (let i = 0; i < matches.length; i++) {
-        const gameId = matches[i].gameId;
-        const home = matches[i].homeTeamNameShort;
-        const away = matches[i].awayTeamNameShort;
-        const startTime = new Date(matches[i].dateTime).getTime();
-
-        contract.storeMatch(home, away, gameId, startTime)
-          .catch(log => {
-            console.log('Error ocurred while storing match: ', log);
-            process.exit();
-          });
-      }
-    });
+  // EatMyBet.deployed()
+  //   .then(contract => {
+  //     for (let i = 0; i < matches.length; i++) {
+  //       const gameId = matches[i].gameId;
+  //       const home = matches[i].homeTeamNameShort;
+  //       const away = matches[i].awayTeamNameShort;
+  //       const startTime = new Date(matches[i].dateTime).getTime();
+  //
+  //       contract.storeMatch(home, away, gameId, startTime)
+  //         .catch(log => {
+  //           console.log('Error ocurred while storing match: ', log);
+  //           process.exit();
+  //         });
+  //     }
+  //   });
 };
